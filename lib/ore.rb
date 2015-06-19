69557735b1439801ff672c7539b5a6895d9a7601
Junichi Ito <jit@sonicgarden.jp>
2015-06-20 04:31:07 +0900

    Refactoring.

--- lib/ore.rb
+++ lib/ore.rb
@@ -6,23 +6,26 @@ class Ore
   end
 
   def first_name
-    config = YAML.load_file(file_path).map(&:values).to_h.invert
-    if first_name = config[@last_name]
-      first_name
+    return '詐欺かも' if suspicious?
+
+    if File.exists?(file_path)
+      name_table[@last_name] || '(Unknown)'
     else
-      @last_name == 'オレオレ' ? '詐欺かも' : '(Unknown)'
+      puts "#{file_path} is not found."
     end
-  rescue Errno::ENOENT
-    print_message file_path
   end
 
   private
+
+  def suspicious?
+    @last_name == 'オレオレ'
+  end
 
   def file_path
     File.expand_path("../../config/ore.yml", __FILE__)
   end
 
-  def print_message(filepath)
-    puts "'#{filepath}' is not found."
+  def name_table
+    YAML.load_file(file_path).map(&:values).to_h.invert
   end
 end
--- spec/ore_spec.rb
+++ spec/ore_spec.rb
@@ -4,46 +4,33 @@ require 'spec_helper'
 describe Ore do
   describe '#first_name' do
     let(:ore) { Ore.new(last_name) }
-    let(:last_name) { nil }
     subject { ore.first_name }
+
     context 'when Matsuyama' do
       let(:last_name) { 'Matsuyama' }
       it { is_expected.to eq 'Hideyuki' }
     end
+
     context 'when 松山' do
       let(:last_name) { '松山' }
       it { is_expected.to eq '秀行' }
     end
+
     context 'when Foo' do
       let(:last_name) { 'Foo' }
       it { is_expected.to eq '(Unknown)' }
     end
-    context 'when だれだれ' do
-      let(:last_name) { 'だれだれ' }
-      it { is_expected.to eq '(Unknown)' }
-    end
-    context 'when Foo' do
-      let(:last_name) { 'Foo' }
-      it { is_expected.to eq '(Unknown)' }
-    end
-    context 'when file_path is nil' do
-      before do
-        allow(ore).to receive(:file_path).and_return(nil)
-      end
-      it 'raises error' do
-        expect{ore.first_name}.to raise_error(TypeError)
-      end
-    end
-    context 'when file does not exist' do
+
+    context 'when file is missing' do
       let(:last_name) { '松山' }
       before do
         path = File.expand_path("../../config/foo.yml", __FILE__)
         allow(ore).to receive(:file_path).and_return(path)
       end
       it { is_expected.to be_nil }
-      it 'gives message' do
+      it 'prints message' do
         expect{ore.first_name}.to \
-          output(/foo\.yml' is not found\./).to_stdout
+          output(/foo\.yml is not found\./).to_stdout
       end
     end
   end
